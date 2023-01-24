2589ed9fef2aa4c407b6ab1770bbfbac38e16d61
Theiket <85735140+Theiket@users.noreply.github.com>
2023-01-24 23:19:20 +1000

    1 file modified, 1 added, 1 deleted

---
+++ components/solarsystem.vue
@@ -0,0 +1,36 @@
+<template>
+<body>
+<div class="container">
+    <div>
+      <h1 class="title">
+        Loadout testing goes here
+      </h1>
+    </div>
+  </div>
+
+  <style scoped>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  background-color:var(--backgray);
+}
+
+.title {
+  width:1200px;
+  animation: 1s appear;
+  font: bold 64px/70px 'Segoe UI', sans-serif;
+  display: block;
+  color: var(--lightorange);
+  letter-spacing: 1px;
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+}
+</style>
