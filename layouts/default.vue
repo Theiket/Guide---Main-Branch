6262d3130e0359829a89379121328cb5fe76d4d0
Theiket <85735140+Theiket@users.noreply.github.com>
2023-01-24 12:44:47 +1000

    2 files modified, 1 added

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,8 @@
 <template>
-<div class="topmenu">
-  <header>
-    <nav>
-    <table>
+  <div class="topmenu">
+    <header>
+      <nav>
+      <table>
       <td>
       <NuxtLink to="/">
         <h1 class="menu">
@@ -39,10 +39,29 @@
         </div>
         </div>
         </td>
-    </table>
-  </nav>
-</header>
-</div>
+        </table>
+      </nav>
+    </header>
+
+    <div>
+      <slot />
+    </div>
+    <div class="bottomcontainer">
+    <div>
+      <h3 class="footer">
+      <table class="cells">
+      <td class="copyright">
+        Copyright 2023 Grindstone Mining Co.
+      </td>
+      <td class="version">
+        GUIDE v0.1-HADES | Updated for Star Citizen Alpha v3.17.4-Live
+      </td>
+      </table>
+      </h3>
+    </div>
+    </div>
+
+  </div>
 </template>
 
 <style scoped>
@@ -62,6 +81,7 @@
   padding-block-start:5px;
   font: bold 45px 'Segoe UI', sans-serif;
   text-decoration:none;
+  display:inline-flex;
 }
 
 @keyframes appear {
@@ -95,7 +115,34 @@
   color:#CF9A2F;
 }
 
-.nuxt-link-active {
+.router-link-exact-active {
   text-decoration:none;
 }
+
+.bottomcontainer {
+  width:auto;
+  padding-top:20px%;
+  margin:auto;
+  display: flex;
+  vertical-align:bottom;
+  justify-content:center;
+  align-items: center;
+  text-align: center;
+  background-color:#24262B;
+}
+
+.footer {
+  font: bold 10px 'Segoe UI', sans-serif;
+  display: inline-block;
+  color: #9DA1B2;
+  letter-spacing: 1px;
+}
+.cells {
+}
+.copyright {
+  width:800px;
+}
+.version {
+  width:800px;
+
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <headermenu />
     <hometext />
     <bottomtext />
   </div>
