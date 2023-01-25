b3265770335007a0d914e21eba858a1844d8ab4c
Theiket <85735140+Theiket@users.noreply.github.com>
2023-01-25 17:36:12 +1000

    1 file added

---
+++ error.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class "mt-7 max-w-sm mx-auto text-center">
+      <p class="mt-7 text-7x1 font-bold">{{ error.statusCode }}</p>
+      <p class="mt-7 text-6x1">Content could not be found.</p>
+      <p class="mt-7">{{ error.message }}</p>
+    </div>
+</template>
+
+<script setup>
++ definePageMeta({ layout: 'default' })
+  defineProps(['error'])
+</script>
+
+<style scoped>
+
+</style>
