f099e9d629816198cfbf3ba7eff4ce9fb2aac5c5
Theiket <85735140+Theiket@users.noreply.github.com>
2023-03-02 04:26:53 +1000

    5 files modified

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,9 @@ export default defineNuxtConfig({
     'nuxt-icon',
     '@vuestic/nuxt',
   ],
+  buildModules: [
+    'mathjs',
+  ],
   app: {
     head: {
       title: 'GUIDE',
--- utils/depositGenerator.js
+++ utils/depositGenerator.js
@@ -1,3 +1,5 @@
+import { random, ceil, log, reduce, round } from 'mathjs';
+
 // Define a function that generates a deposit
 function generateDeposit(minerals, minCount) {
   // Initialize an empty deposit object
@@ -5,39 +7,27 @@ function generateDeposit(minerals, minCount) {
   // Initialize a variable to keep track of the total count of minerals
   let totalCount = 0;
 
-  // Iterate over each mineral type in the given array
-  minerals.forEach(({ name, probability, exponent, minPercentage, maxPercentage }) => {
-    // Initialize a variable to keep track of the count of the current mineral type
-    let count = 0;
+  // Check if minerals is an array before calling forEach
+  if (Array.isArray(minerals)) {
+    minerals.forEach((mineral) => {
+      const percentage = calculatePercentage(mineral.minPercentage, mineral.maxPercentage, mineral.probability, mineral.exponent);
+      depositMinerals.push({
+        name: mineral.name,
+        percentage: percentage.toFixed(2),
+      });
+    });
+  } else {
+    console.error('minerals is not an array:', minerals);
+  }
 
-    // If a minimum count is specified and the total count of minerals is less than the minimum count
-    if (minCount && totalCount < minCount) {
-      // Calculate the count of the current mineral type based on its minimum percentage
-      count = ceil((minCount * minPercentage) / 100);
-      // Increment the total count of minerals
-      totalCount += count;
-    }
-    // If the total count of minerals is still less than the minimum count and this mineral type hasn't been added yet
-    else if (totalCount < minCount) {
-      // Calculate a random value between 0 and 1
-      const randValue = random();
-      // If the random value is less than the mineral's probability
-      if (randValue / probability < 1) {
-        // Calculate the count of the current mineral type based on the exponential distribution
-        count = ceil(
-          (-exponent * log(random())) * ((maxPercentage - minPercentage) / exponent) + minPercentage
-        );
-        // Increment the total count of minerals
-        totalCount += count;
-      }
-    }
-    // If the count of the current mineral type is greater than 0
-    if (count > 0) {
-      // Add the current mineral type to the deposit object
-      deposit.minerals.push({ name, count, percentage: 0 });
-    }
-  });
-
+  // Check if depositMinerals has enough items before returning it
+  if (depositMinerals.length >= minCount) {
+    return { minerals: depositMinerals };
+  } else {
+    console.error('Could not generate deposit with enough minerals');
+    return null;
+  }
+  
   // Calculate the total percentage of all the mineral types in the deposit
   const totalPercentage = reduce(deposit.minerals, (sum, { count }) => sum + count, 0);
 
@@ -60,6 +50,20 @@ function generateDeposit(minerals, minCount) {
 
   // Return the generated deposit
   return deposit;
+}
+
+// Define a function that calculates the percentage of a mineral type in a deposit
+function calculatePercentage(minPercentage, maxPercentage, probability, exponent) {
+  // Calculate a random value between 0 and 1
+  const randValue = random();
+  // If the random value is less than the mineral's probability
+  if (randValue / probability < 1) {
+    // Calculate the percentage of the mineral type based on the exponential distribution
+    const percentage = ceil((-exponent * log(random())) * ((maxPercentage - minPercentage) / exponent) + minPercentage);
+    return percentage;
+  }
+  // If the random value is greater than the mineral's probability, return 0
+  return 0;
 }
 
 // Export the generateDeposit function
