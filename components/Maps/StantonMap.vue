72ab53199f9a9ef8c3ee2f8aabddd0d6fe147fd2
Theiket <85735140+Theiket@users.noreply.github.com>
2023-03-05 18:37:01 +1000

    2 files modified

--- components/Maps/StantonMap.vue
+++ components/Maps/StantonMap.vue
@@ -49,9 +49,6 @@
 .systemName {
   animation: 1.5s slideappear;
 }
-.solar-system {
-  animation: 5s appear;
-}
 
 .solar-system {
   width: 250px;
@@ -66,152 +63,221 @@
 }
 
 /* Orbit */
-.orbit {
-	position: absolute;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	border: 1px solid var(--lightorange);
-	border-radius: 50%;
-  z-index:0;
-} 
-.microtech-orbit {
-  width: 210px;
-	height: 210px;
-  rotate:50deg;
-}
-.arccorp-orbit {
-	width: 165px;
-	height: 165px;
-  rotate:135deg;
-}
-.crusader-orbit {
-	width: 120px;
-	height: 120px;
-  rotate:240deg;
-}
-.hurston-orbit {
-	width: 90px;
-	height: 90px;
-  rotate:130deg;
-}
+  .orbit {
+    position: absolute;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid var(--lightorange);
+    border-radius: 50%;
+    z-index:-1;
+  } 
+  .microtech-orbit {
+    width: 210px;
+    height: 210px;
+    rotate:50deg;
+  }
+  .arccorp-orbit {
+    width: 165px;
+    height: 165px;
+    rotate:135deg;
+  }
+  .crusader-orbit {
+    width: 120px;
+    height: 120px;
+    rotate:240deg;
+  }
+  .hurston-orbit {
+    width: 90px;
+    height: 90px;
+    rotate:130deg;
+  }
 
 /* Planets */
-.planet {
-	position: absolute;
-	top: -5px;
-  width: 10px;
-  height: 10px;
-	border-radius: 50%;
-  background-color: #3ff9dc;
-  z-index:1;
-}
-.planet.hurston {
-  background-color:#CF3A2F;
-  rotate:-130deg;
-}
-.planet.crusader {
-  background-color:#2FCFA9;
-  rotate:-240deg;
-}
-.planet.arccorp {
-  background-color:#CF2F55;
-  rotate:-135deg;
-}
-.planet.microtech {
-  background-color:white;
-  rotate:-50deg;
-}
-.planet:hover {
-  box-shadow:0px 0px 5px black;
-  top:-6.5px;
-  width:13px;
-  height:13px;
-}
-.planet p {
-  color:transparent;
-  z-index:2;
-  rotate:0deg;
-  margin-inline-start:-20px;
-  margin-block-start:-3px;
-  letter-spacing:0.5px;
-}
-.planet:hover p {
-  color:var(--lightgray);
-}
+  .planet {
+    position: absolute;
+    top: -5px;
+    width: 10px;
+    height: 10px;
+    border-radius: 50%;
+    background-color: #3ff9dc;
+    z-index:1;
+  }
+  .planet.hurston {
+    background-color:#CF3A2F;
+    rotate:-130deg;
+  }
+  .planet.crusader {
+    background-color:#2FCFA9;
+    rotate:-240deg;
+  }
+  .planet.arccorp {
+    background-color:#CF2F55;
+    rotate:-135deg;
+  }
+  .planet.microtech {
+    background-color:white;
+    rotate:-50deg;
+  }
+  .planet:hover {
+    box-shadow:0px 0px 5px black;
+    top:-6.5px;
+    width:13px;
+    height:13px;
+    cursor:pointer;
+  }
+  .planet p {
+    color:transparent;
+    z-index:2;
+    rotate:0deg;
+    margin-inline-start:-20px;
+    margin-block-start:-3px;
+    letter-spacing:0.5px;
+  }
+  .planet:hover p {
+    color:var(--lightgray);
+  }
 
 /* Sun */
-.sun {
-	width: 30px;
-	height: 30px;
-	border-radius: 50%;
-	background-color: #CF9A2F;
-}
-.sun:hover {
-  box-shadow:0px 0px 5px black;
-  width:35px;
-  height:35px;
-}
+  .sun {
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+    background-color: #CF9A2F;
+    z-index:1;
+  }
+  .sun:hover {
+    box-shadow:0px 0px 5px black;
+    width:35px;
+    height:35px;
+    cursor:pointer;
+  }
 
 /* Text */
-.systemName {
-  color: var(--lightorange);
-  letter-spacing:1px;
-  margin-block-start:0px;
-  text-decoration:none;
-  display:inline-block;
-  width:15%;
-  position:absolute;
-  margin-inline-start:20%;
-  margin-block-start:5%;
-  border-bottom-style:solid;
-  border-width:2px;
-  border-color:var(--lightgray);
-  padding-inline-start:10px;
-  z-index:1;
-}
-.systemName h1 {
-  font: 35px 'Segoe UI', sans-serif;
-  font-weight:bold;
-}
-.systemName a {
-  color:var(--lightorange);
-}
-.systemName a:hover {
-  color:var(--orangehover);
-}
+  .systemName {
+    color: var(--lightorange);
+    letter-spacing:1px;
+    margin-block-start:0px;
+    text-decoration:none;
+    display:inline-block;
+    width:15%;
+    position:absolute;
+    margin-inline-start:20%;
+    margin-block-start:5%;
+    border-bottom-style:solid;
+    border-width:2px;
+    border-color:var(--lightgray);
+    padding-inline-start:10px;
+    z-index:1;
+  }
+  .systemName h1 {
+    font: 35px 'Segoe UI', sans-serif;
+    font-weight:bold;
+  }
+  .systemName a {
+    color:var(--lightorange);
+  }
+  .systemName a:hover {
+    color:var(--orangehover);
+  }
 
-p {
-  font-weight: 300;
-  font-family: 'Segoe UI', sans-serif;
-  font-size: 5px;
-  font-weight:bold;
-  letter-spacing:0.3px;
-  color: var(--lightgray);
-  padding-block-end: 15px;
-  padding-inline-start:15px;
-  padding-inline-end:15px;
-  rotate:-140deg;
-}
+  p {
+    font-weight: 300;
+    font-family: 'Segoe UI', sans-serif;
+    font-size: 5px;
+    font-weight:bold;
+    letter-spacing:0.3px;
+    color: var(--lightgray);
+    padding-block-end: 15px;
+    padding-inline-start:15px;
+    padding-inline-end:15px;
+    rotate:-140deg;
+  }
 
 /* Animations */
-@keyframes slideappear {
-  0% {
-    opacity: 0;
-    left:-500px;
+  @keyframes slideappear {
+    0% {
+      opacity: 0;
+      left:-500px;
+    }
+    75% {
+      opacity:0;
+    }
+    100% {
+      opacity: 1;
+      left:25px;
+    }
   }
-  75% {
-    opacity:0;
-  }
-  100% {
-    opacity: 1;
-    left:25px;
-  }
-}
 
-@keyframes appear {
-  0% {
-    opacity: 0;
+  @keyframes appear1 {
+    0% {
+      opacity: 0;
+      scale:0.3;
+      }
+    60% {
+      opacity:0;
+      scale:0.6;
+      }
+    }
+  @keyframes appear2 {
+    0% {
+      opacity: 0;
+      scale:0;
+      }
+    60% {
+      opacity:0;
+      scale:0.6;
+      rotate:0deg;
+      }
+    }
+  @keyframes appear3 {
+    0% {
+      opacity: 0;
+      scale:0;
+      }
+    60% {
+      opacity:0;
+      scale:0.6;
+      rotate:0deg;
+      }
+    }
+  @keyframes appear4 {
+    0% {
+      opacity: 0;
+      scale:0;
+      }
+    60% {
+      opacity:0;
+      scale:0.6;
+      rotate:0deg;
+      }
+    }
+  @keyframes appear5 {
+    0% {
+      opacity: 0;
+      scale:0.3;
+      }
+    80% {
+      opacity:0;
+      scale:0.8;
+      rotate:0deg;
+      }
+    }
+
+  .sun {
+    animation: 1s appear1;
   }
-}
+  .hurston-orbit {
+    animation: 2s appear2;
+  }
+  .crusader-orbit {
+    animation: 3s appear3;
+  }
+  .arccorp-orbit {
+    animation: 4s appear4;
+  }
+  .microtech-orbit {
+    animation: 4.5s appear5;
+  }
+
 </style>
--- components/starmap/Stanton.vue
+++ components/starmap/Stanton.vue
@@ -1,87 +1,97 @@
 <template>
-  <transition name="fade">
-    <NuxtLink to="/MapStanton">
-      <div class="systemStanton">
-        <Icon name="mdi:circle-slice-8" />
-        <br>
-        <p class="planetText">STANTON</p>
-        <p class="systemText">SYSTEM</p>
-      </div>
-    </NuxtLink>
-  </transition>
+  <div id="solar-system" class="systemStanton" @click="zoom()">
+    <Icon name="mdi:circle-slice-8" />
+    <br>
+    <p class="planetText">STANTON</p>
+    <p class="systemText">SYSTEM</p>
+  </div>
 </template>
 
 <script>
+export default {
+  methods: {
+    zoom() {
+      const element = document.getElementById('solar-system')
+      element.style.transition = 'all 0.5s ease-in-out'
+      element.style.opacity = '0'
+      element.style.transform = 'scale(3)'
+      setTimeout(() => {
+        this.$router.push('/MapStanton')
+      }, 500)
+    }
+  }
+}
 </script>
 
 <style scoped>
 .systemStanton {
-  position:relative;
+  position: relative;
   height: 50px;
   width: 50px;
   color: var(--lightgray);
   border-radius: 50%;
-  margin:10px 10px;
-  user-select:none;
+  margin: 10px 10px;
+  user-select: none;
 }
+
 .systemStanton:hover {
   color: var(--greensafety);
+  cursor:pointer;
 }
 
 .systemStanton .planetText {
   font: bold 15px 'Segoe UI', sans-serif;
-  letter-spacing:1px;
-  text-transform:capitalize;
-  opacity:1;
-  position:relative;
-  color:var(--lightorange);
+  letter-spacing: 1px;
+  text-transform: capitalize;
+  opacity: 1;
+  position: relative;
+  color: var(--lightorange);
   -webkit-transition: all 400ms ease-out;
   -moz-transition: all 400ms ease-out;
   -o-transition: all 400ms ease-out;
   -ms-transition: all 400ms ease-out;
   transition: all 400ms ease-out;
-  width:100px;
-  padding-block-start:15px;
+  width: 100px;
+  padding-block-start: 15px;
 }
-.systemStanton:hover .planetText{
-  opacity:1;
+
+.systemStanton:hover .planetText {
+  opacity: 1;
 }
 
 .systemStanton .systemText {
   font: 550 15px 'Segoe UI', sans-serif;
-  letter-spacing:1px;
-  text-transform:capitalize;
-  opacity:0.7;
-  position:relative;
-  color:var(--lightorange);
+  letter-spacing: 1px;
+  text-transform: capitalize;
+  opacity: 0.7;
+  position: relative;
+  color: var(--lightorange);
   -webkit-transition: all 400ms ease-out;
   -moz-transition: all 400ms ease-out;
   -o-transition: all 400ms ease-out;
   -ms-transition: all 400ms ease-out;
   transition: all 400ms ease-out;
-  width:100px;
+  width: 100px;
 }
 
 .icon {
-  scale:300%;
-  border-style:dashed;
-  border-color:transparent;
-  border-width:thin;
-  border-radius:50%;
+  scale: 300%;
+  border-style: dashed;
+  border-color: transparent;
+  border-width: thin;
+  border-radius: 50%;
 }
+
 .systemStanton:hover .icon {
-  border-style:dashed;
-  border-color:var(--orangehover);
-  border-width:thin;
-  border-radius:50%;
-  animation:1s appear;
-}
-.fade-enter-active, .fade-leave-active {
-  transition: opacity 0.5s;
+  border-style: dashed;
+  border-color: var(--orangehover);
+  border-width: thin;
+  border-radius: 50%;
+  animation: 1s appear;
 }
 
-.fade-enter, .fade-leave-to {
-  opacity: 0;
+.router-link-active {
+  transition: all 0.5s ease-in-out;
+  transform: scale(1.2);
 }
-
 </style>
