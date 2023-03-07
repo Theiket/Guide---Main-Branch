50ba7242d0f8890610eaeb8efcaf84bf9b047e52
Theiket <85735140+Theiket@users.noreply.github.com>
2023-03-08 01:11:08 +1000

    1 file modified

--- components/Maps/Planets/Crusader/CrusaderMap.vue
+++ components/Maps/Planets/Crusader/CrusaderMap.vue
@@ -5,7 +5,8 @@
 </template>
 
 <script>
-import * as THREE from 'three'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 export default {
   data() {
@@ -47,7 +48,7 @@ export default {
       const geometry = new THREE.PlaneGeometry(width / 32, height / 32)
       const material = new THREE.MeshBasicMaterial({ map: texture, transparent: true })
       const mesh = new THREE.Mesh(geometry, material)
-
+      // Return mesh
       return mesh
     }
   },
@@ -56,7 +57,7 @@ export default {
     this.scene = new THREE.Scene()
     this.scene.background = new THREE.Color(0x24262B)
     this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-    this.camera.position.z = 70
+    this.camera.position.z = 50
     this.camera.position.x = 0
     this.renderer = new THREE.WebGLRenderer({ canvas: this.$refs.canvas })
     this.renderer.setSize(window.innerWidth, window.innerHeight)
@@ -69,13 +70,19 @@ export default {
     this.scene.add(directionalLight)
   // Create the star mesh objects and their name labels
     this.starSystems.forEach(starSystem => {
+      // Create the stars for each object in starSystem
       const starGeometry = new THREE.SphereGeometry(1, 32, 32)
-      const starMaterial = new THREE.MeshBasicMaterial({ color: starSystem.color })
+      const starMaterial = new THREE.PointsMaterial({
+        size: 0.5,
+        sizeAttenuation: true,
+        color: starSystem.color,
+        alphaTest: 0.5
+      })
       const starMesh = new THREE.Mesh(starGeometry, starMaterial)
       starMesh.position.copy(starSystem.position)
       starMesh.name = starSystem.name
       this.scene.add(starMesh)
-
+    // Create the name labels
       const nameLabel = this.createLabel(starSystem.name, 0x000000)
       nameLabel.position.copy(starSystem.position)
       nameLabel.position.y += 2
@@ -83,63 +90,59 @@ export default {
       starMesh.nameLabel = nameLabel
       this.scene.add(nameLabel)
     });
-  // Add click event listener to the star mesh
-    this.$refs.canvas.addEventListener('click', () => {
+  // Functions for eventListeners
+    function handleIntersection(event, camera, raycaster, scene, intersectedObject, mouse) {
     // Get the canvas position
-      const canvasBounds = this.$refs.canvas.getBoundingClientRect()
+      const canvasBounds = event.currentTarget.getBoundingClientRect()
     // Calculate normalized device coordinates
-      this.mouse.x = ((event.clientX - canvasBounds.left) / canvasBounds.width) * 2 - 1
-      this.mouse.y = -((event.clientY - canvasBounds.top) / canvasBounds.height) * 2 + 1
+      mouse.x = ((event.clientX - canvasBounds.left) / canvasBounds.width) * 2 - 1
+      mouse.y = -((event.clientY - canvasBounds.top) / canvasBounds.height) * 2 + 1
     // Update the picking ray with the camera and mouse position
-      this.raycaster.setFromCamera(this.mouse, this.camera)
+      raycaster.setFromCamera(mouse, camera)
     // Check for intersections
-      const intersects = this.raycaster.intersectObjects(this.scene.children.filter(child => child.nameLabel))
+      const intersects = raycaster.intersectObjects(scene.children.filter(child => child.nameLabel))
       if (intersects.length > 0) {
-        if (this.intersectedObject !== intersects[0].object) {
+        if (intersectedObject !== intersects[0].object) {
         // Mouse entered a new object
-          if (this.intersectedObject) {
+          if (intersectedObject) {
+            intersectedObject.material.color.set(0xffffff)
+            intersectedObject.nameLabel.visible = false
           }
-          this.intersectedObject = intersects[0].object
+          intersectedObject = intersects[0].object
+          intersectedObject.material.color.set(0x0046F5)
+          intersectedObject.nameLabel.visible = true
         }
       } else {
-        // Mouse left the object
-        if (this.intersectedObject) {
-          this.intersectedObject = null
+      // Mouse left the object
+        if (intersectedObject) {
+          intersectedObject.material.color.set(0xffffff)
+          intersectedObject.nameLabel.visible = false
+          intersectedObject = null
         }
       }
+      return intersectedObject
+    }
+
+  // Add click event listener to the canvas
+    this.$refs.canvas.addEventListener('click', (event) => {
+      this.intersectedObject = handleIntersection(event, this.camera, this.raycaster, this.scene, this.intersectedObject, this.mouse)
       // TODO: Expand into solar system
     })
+
   // Add mousemove event listener to the canvas
     this.$refs.canvas.addEventListener('mousemove', (event) => {
-    // Get the canvas position
-      const canvasBounds = this.$refs.canvas.getBoundingClientRect()
-    // Calculate normalized device coordinates
-      this.mouse.x = ((event.clientX - canvasBounds.left) / canvasBounds.width) * 2 - 1
-      this.mouse.y = -((event.clientY - canvasBounds.top) / canvasBounds.height) * 2 + 1
-    // Update the picking ray with the camera and mouse position
-      this.raycaster.setFromCamera(this.mouse, this.camera)
-    // Check for intersections
-      const intersects = this.raycaster.intersectObjects(this.scene.children.filter(child => child.nameLabel))
-      if (intersects.length > 0) {
-        if (this.intersectedObject !== intersects[0].object) {
-        // Mouse entered a new object
-          if (this.intersectedObject) {
-            this.intersectedObject.material.color.set(0xffffff)
-            this.intersectedObject.nameLabel.visible = false
-          }
-          this.intersectedObject = intersects[0].object
-          this.intersectedObject.material.color.set(0x0046F5)
-          this.intersectedObject.nameLabel.visible = true
-        }
-      } else {
-        // Mouse left the object
-        if (this.intersectedObject) {
-          this.intersectedObject.material.color.set(0xffffff)
-          this.intersectedObject.nameLabel.visible = false
-          this.intersectedObject = null
-        }
-      }
+      this.intersectedObject = handleIntersection(event, this.camera, this.raycaster, this.scene, this.intersectedObject, this.mouse)
     })
+
+  // Add interactivity
+      this.controls = new OrbitControls(this.camera, this.$refs.canvas);
+      this.controls.enableDamping = true;
+      this.controls.dampingFactor = 0.05;
+      this.controls.rotateSpeed = 0.5;
+      this.controls.maxDistance = 1000;
+      this.controls.minDistance = 10;
+      this.controls.enableZoom = true;
+
   // Render the scene
     this.animate = () => {
       requestAnimationFrame(this.animate)
