e0ba48be3a324130a39a7cf0b4682827f57a7ad7
Theiket <85735140+Theiket@users.noreply.github.com>
2023-03-07 14:23:58 +1000

    1 file modified

--- components/Maps/Planets/Crusader/CrusaderMap.vue
+++ components/Maps/Planets/Crusader/CrusaderMap.vue
@@ -1,7 +1,5 @@
 <template>
-  <div class="container">
-    <canvas ref="canvas" />
-  </div>
+  <div ref="container"></div>
 </template>
 
 <script>
@@ -9,204 +7,148 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 export default {
+  data() {
+    return {
+      scene: null,
+      camera: null,
+      renderer: null,
+      controls: null,
+      starSystems: [],
+      raycaster: new THREE.Raycaster(),
+      mouse: new THREE.Vector2(),
+      isDragging: false,
+      intersectedObject: null,
+    };
+  },
   mounted() {
+    // Initialize the THREE.js scene
     this.init();
+    this.createStarSystems();
+    this.addEventListeners();
+    this.render();
   },
   methods: {
     init() {
-    // Set up the renderer
-      this.renderer = new THREE.WebGLRenderer({ canvas: this.$refs.canvas });
-      this.renderer.setSize(window.innerWidth, window.innerHeight);
-      this.renderer.setPixelRatio(window.devicePixelRatio);
-
-    // Set up the camera
-      this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-      this.camera.position.z = 70;
-
-    // Set up the scene
+      // Initialize the THREE.js scene
       this.scene = new THREE.Scene();
-      this.scene.background = new THREE.Color(0x24262B);
 
-    // Add lighting
-      const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-      this.scene.add(ambientLight);
+      // Initialize the camera
+      this.camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+      );
+      this.camera.position.set(0, 0, 50);
 
-      const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
-      this.scene.add(directionalLight);
-    
-    // Create the sun
-      const sunGeometry = new THREE.SphereGeometry(5, 128, 128);
-      const sunMaterial = new THREE.MeshPhongMaterial({
-        emissive: 0xfdb813,
-        emissiveIntensity: 1,
-        map: new THREE.TextureLoader().load('https://threejsfundamentals.org/threejs/resources/images/lava/lavatile.jpg'),
-        shininess: 10,
-        specular: 0xfdb813
-      });
-      this.sun = new THREE.Mesh(sunGeometry, sunMaterial);
-      this.sun.position.set(0, 0, 0);
-      this.scene.add(this.sun);
+      // Initialize the renderer
+      this.renderer = new THREE.WebGLRenderer();
+      this.renderer.setSize(window.innerWidth, window.innerHeight);
+      this.$refs.container.appendChild(this.renderer.domElement);
 
-      // Add a point light to the sun
-      const pointLight = new THREE.PointLight(0xfdb813, 1, 100);
-      pointLight.position.set(0, 0, 0);
-      this.sun.add(pointLight);
-
-      // Create the sun glow effect
-      const sunGlowGeometry = new THREE.SphereGeometry(6, 64, 64);
-      const sunGlowMaterial = new THREE.ShaderMaterial({
-        uniforms: {
-          glowColor: { value: new THREE.Color(0xfdb813) },
-          viewVector: { value: new THREE.Vector3() }
-        },
-        vertexShader: `
-          varying vec3 vNormal;
-          void main() 
-          {
-            vNormal = normalize(normalMatrix * normal);
-            gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-          }
-        `,
-        fragmentShader: `
-          uniform vec3 glowColor;
-          varying vec3 vNormal;
-          void main() 
-          {
-            float intensity = pow(0.4 - dot(vNormal, vec3(0, 0, 1.0)), 2.0);
-            gl_FragColor = vec4(glowColor * intensity, intensity);
-          }
-        `,
-        side: THREE.BackSide,
-        blending: THREE.AdditiveBlending,
-        transparent: true
-      });
-      this.sunGlow = new THREE.Mesh(sunGlowGeometry, sunGlowMaterial);
-      this.sunGlow.position.set(0, 0, 0);
-      this.scene.add(this.sunGlow);
-
-      // Create a solar flare
-      const flareGeometry = new THREE.ConeGeometry(0.4, 4, 32);
-      const flareMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff, transparent: true, opacity: 0.8 });
-      const flare = new THREE.Mesh(flareGeometry, flareMaterial);
-      flare.position.set(1, 0, 0);
-      this.sun.add(flare);
-
-      // Create a prominence
-      const prominenceGeometry = new THREE.TorusGeometry(1.5, 0.1, 16, 100);
-      const prominenceMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000, transparent: true, opacity: 0.8 });
-      const prominence = new THREE.Mesh(prominenceGeometry, prominenceMaterial);
-      prominence.position.set(-2, 0, 0);
-      this.sun.add(prominence);
-
-    // Create planet #1
-      const hurstonGeometry = new THREE.SphereGeometry(2, 32, 32);
-      const hurstonMaterial = new THREE.MeshStandardMaterial({
-      color: 0xCF3A2F,
-      wireframe: false,
-      wireframeLinecap: "square",
-      wireframeLinejoin: "miter",
-      roughness: 0.8,
-      flatShading: false,
-    });
-      this.hurston = new THREE.Mesh(hurstonGeometry, hurstonMaterial);
-      this.hurston.position.x = 17;
-      this.scene.add(this.hurston);
-
-    // Create planet #2
-      const crusaderGeometry = new THREE.SphereGeometry(2, 32, 32);
-      const crusaderMaterial = new THREE.MeshStandardMaterial({
-      color: 0x2FCFA9,
-      wireframe: false,
-      wireframeLinecap: "square",
-      wireframeLinejoin: "miter",
-      roughness: 0.8,
-      flatShading: false,
-    });
-      this.crusader = new THREE.Mesh(crusaderGeometry, crusaderMaterial);
-      this.crusader.position.x = 25;
-      this.crusader.position.z = 0;
-      this.scene.add(this.crusader);
-
-    // Create planet #3
-      const arccorpGeometry = new THREE.SphereGeometry(2, 32, 32);
-      const arccorpMaterial = new THREE.MeshStandardMaterial({
-      color: 0xCF2F55,
-      wireframe: false,
-      wireframeLinecap: "square",
-      wireframeLinejoin: "miter",
-      roughness: 0.8,
-      flatShading: false,
-    });
-      this.arccorp = new THREE.Mesh(arccorpGeometry, arccorpMaterial);
-      this.arccorp.position.x = 34;
-      this.arccorp.position.z = 0;
-      this.scene.add(this.arccorp);
-
-    // Create planet #4
-      const microtechGeometry = new THREE.SphereGeometry(2, 32, 32);
-      const microtechMaterial = new THREE.MeshStandardMaterial({
-      color: 0xfdfdfd,
-      wireframe: false,
-      wireframeLinecap: "square",
-      wireframeLinejoin: "miter",
-      roughness: 0.8,
-      flatShading: false,
-    });
-      this.microtech = new THREE.Mesh(microtechGeometry, microtechMaterial);
-      this.microtech.position.x = 50;
-      this.microtech.position.z = 0;
-      this.scene.add(this.microtech);
-
-    // Add interactivity
-      this.controls = new OrbitControls(this.camera, this.$refs.canvas);
+      // Initialize the controls
+      this.controls = new OrbitControls(this.camera, this.renderer.domElement);
       this.controls.enableDamping = true;
       this.controls.dampingFactor = 0.05;
       this.controls.rotateSpeed = 0.5;
-      this.controls.maxDistance = 100;
-      this.controls.minDistance = 10;
-      this.controls.enableZoom = true;
-
-    // Start animation loop
-      this.animate();
     },
-    animate() {
-      requestAnimationFrame(this.animate);
+    createStarSystems() {
+      // Create the star systems
+      const systemPositions = [
+        { x: -20, y: 0, z: 0 },
+        { x: 20, y: 0, z: 0 },
+        { x: 0, y: -20, z: 0 },
+        { x: 0, y: 20, z: 0 },
+      ];
 
-    // Animate solar system objects
-      this.sun.rotation.y += 0.001;
-      this.hurston.rotation.y += 0.005;
-      this.crusader.rotation.y += 0.001;
-      this.arccorp.rotation.y += 0.003;
-      this.microtech.rotation.y += 0.002;
+      const starGeometry = new THREE.CircleGeometry(10, 32);
+      const starMaterial = new THREE.PointsMaterial({ color: 0xffffff });
 
-    // Update controls
-      this.controls.update();
+      for (let i = 0; i < systemPositions.length; i++) {
+        const { x, y, z } = systemPositions[i];
 
-    // Render scene
-      this.renderer.render(this.scene, this.camera);
+        const star = new THREE.Points(starGeometry, starMaterial);
+        star.position.set(x, y, z);
+        this.scene.add(star);
+
+        // Store the star system position for later use
+        const worldPos = new THREE.Vector3();
+        worldPos.setFromMatrixPosition(star.matrixWorld);
+        this.starSystemPositions.push(worldPos);
+      }
     },
+    addEventListeners() {
+      // Add event listeners for interactivity
+      window.addEventListener('resize', this.onWindowResize);
+      this.$refs.container.addEventListener('mousedown', this.onMouseDown);
+      this.$refs.container.addEventListener('mouseup', this.onMouseUp);
+    },
+
     onWindowResize() {
-    // Adjust canvas size on window resize
+      // Resize the renderer and update the camera aspect ratio
       this.camera.aspect = window.innerWidth / window.innerHeight;
       this.camera.updateProjectionMatrix();
       this.renderer.setSize(window.innerWidth, window.innerHeight);
     },
-  },
-  mounted() {
-    this.init();
-    window.addEventListener('resize', this.onWindowResize);
-  },
-  beforeDestroy() {
-    window.removeEventListener('resize', this.onWindowResize);
-  },
-};
+
+
+    onMouseDown(event) {
+      // Set the dragging flag to true and store the mouse position
+      this.isDragging = true;
+      this.mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+      this.mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+      // Cast a ray from the camera's position in the direction of the mouse click
+      const raycaster = new THREE.Raycaster();
+      raycaster.setFromCamera(this.mouse, this.camera);
+      const intersects = raycaster.intersectObjects(this.starSystems);
+
+      // Check if a star system was clicked on
+      if (intersects.length > 0) {
+        const clickedStarSystem = intersects[0].object;
+        const index = this.starSystems.indexOf(clickedStarSystem);
+        const worldPos = this.starSystemPositions[index];
+
+        // Zoom in on the clicked star system
+        this.controls.target.copy(worldPos);
+        this.controls.update();
+      }
+    },
+
+    onMouseUp(event) {
+      // Set the dragging flag to false
+      this.isDragging = false;
+
+      // Check if an object was intersected and zoom in if it was
+      if (this.intersectedObject) {
+        const position = this.intersectedObject.position.clone();
+        const distance = this.camera.position.distanceTo(position);
+        const duration = Math.min(Math.max(distance / 2000, 1), 2);
+
+        this.camera.position.x = position.x;
+        this.camera.position.y = position.y;
+        this.camera.position.z = position.z;
+
+        this.intersectedObject = null;
+      }
+    },
+
+    render() {
+      // Call render in a loop
+      requestAnimationFrame(this.render);
+
+      // Update the controls and render the scene
+      this.controls.update();
+      this.renderer.render(this.scene, this.camera);
+    },
+  };
+}
 </script>
 
 <style scoped>
-  .container {
-    height:85vh;
+  div {
+    max-width: 100%;
+    max-height: 90vh;
     overflow:hidden;
   }
-
-
 </style>
