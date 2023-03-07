6f5c7b72553f036043bc40e2c2b72d0dacd43015
Theiket <85735140+Theiket@users.noreply.github.com>
2023-03-07 19:22:11 +1000

    1 file modified

--- components/Maps/Planets/Crusader/CrusaderMap.vue
+++ components/Maps/Planets/Crusader/CrusaderMap.vue
@@ -1,153 +1,67 @@
 <template>
   <div ref="container">
-    <canvas> </canvas>
+    <canvas ref="canvas">
+    </canvas>
   </div>
 </template>
 
 <script>
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 export default {
-  data() {
+  data () {
     return {
-      scene: null,
-      camera: null,
-      renderer: null,
-      controls: null,
-      starSystems: [],
-      starSystemPositions: [],
-      raycaster: new THREE.Raycaster(),
-      mouse: new THREE.Vector2(),
-      isDragging: false,
-      intersectedObject: null,
+      starSystems:[],
     };
   },
   mounted() {
-    const container = this.$refs.container;
+      const canvas = this.$refs.canvas
 
-  // Initialize the THREE.js scene
-    this.scene = new THREE.Scene();
-    this.scene.background = new THREE.Color(0x24262B);
-
-  // Initialize the camera
-    this.camera = new THREE.PerspectiveCamera(
-      75,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      1000
-    );
-    this.camera.position.set(0, 0, 50);
-    const cameraPosition = this.camera.position.clone();
-
-  // Initialize the renderer
-    this.renderer = new THREE.WebGLRenderer({ canvas: this.$refs.canvas });
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-    this.renderer.setPixelRatio(window.devicePixelRatio);
-
-  // Add interactivity
-    this.controls = new OrbitControls(this.camera, this.$refs.canvas);
-    this.controls.enableDamping = true;
-    this.controls.dampingFactor = 0.05;
-    this.controls.rotateSpeed = 0.5;
-    this.controls.maxDistance = 100;
-    this.controls.minDistance = 10;
-    this.controls.enableZoom = true;
-
-
-    createStarSystems() {
-  // Create the star systems
-      const systemPositions = [    { x: -20, y: 0, z: 0 },    { x: 20, y: 0, z: 0 },    { x: 0, y: -20, z: 0 },    { x: 0, y: 20, z: 0 },  ];
-
-      const starGeometry = new THREE.CircleGeometry(10, 32);
-      const starMaterial = new THREE.PointsMaterial({ color: 0xffffff });
-
-      for (let i = 0; i < systemPositions.length; i++) {
-        const position = systemPositions[i];
-        const x = position.x;
-        const y = position.y;
-        const z = position.z;
-
-        const star = new THREE.Points(starGeometry, starMaterial);
-        star.position.set(x, y, z);
-        this.scene.add(star);
-
-        // Store the star system position for later use
-        const worldPos = new THREE.Vector3();
-        worldPos.setFromMatrixPosition(star.matrixWorld);
-        this.starSystems.push(star);
-        this.starSystemPositions.push(worldPos);
-      }
-    },
-
-  // Add event listeners for interactivity
-    window.addEventListener('resize', this.onWindowResize);
-    container.addEventListener('mousedown', this.onMouseDown);
-    container.addEventListener('mouseup', this.onMouseUp);
-
-    onWindowResize() {
-  // Resize the renderer and update the camera aspect ratio
-      this.camera.aspect = window.innerWidth / window.innerHeight;
-      this.camera.updateProjectionMatrix();
+    // Create the scene, camera, and renderer
+      this.scene = new THREE.Scene()
+      this.scene.background = new THREE.Color(0x24262B);
+      this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+      this.camera.position.z = 70;
+      this.renderer = new THREE.WebGLRenderer({ canvas: this.$refs.canvas });
       this.renderer.setSize(window.innerWidth, window.innerHeight);
-    },
+      this.renderer.setPixelRatio(window.devicePixelRatio);
 
-    onMouseDown(event) {
-  // Set the dragging flag to true and store the mouse position
-      this.isDragging = true;
-      this.mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-      this.mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+    // Add lighting
+      const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+      this.scene.add(ambientLight);
 
-  // Cast a ray from the camera's position in the direction of the mouse click
-      this.raycaster.setFromCamera(this.mouse, this.camera);
-      const intersects = this.raycaster.intersectObjects(this.starSystems);
+      const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+      this.scene.add(directionalLight);
 
-  // Check if a star system was clicked on
-      if (intersects.length > 0) {
-        const clickedStarSystem = intersects[0].object;
-        const index = this.starSystems.indexOf(clickedStarSystem);
-        const worldPos = this.starSystemPositions[index];
+    // Create the star mesh
+      const starGeometry = new THREE.SphereGeometry(1, 32, 32)
+      const starMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff })
+      const starMesh = new THREE.Mesh(starGeometry, starMaterial)
+      this.scene.add(starMesh)
 
-  // Zoom in on the clicked star system
-        this.controls.target.copy(worldPos);
-        this.controls.update();
-      }
-    },
+    // Add mouseover event listener to the star mesh
+    starMesh.addEventListener('mouseover', () => {
+      starMesh.material.color.set(0x0046F5);
+    })
 
-    onMouseUp(event) {
-  // Set the dragging flag to false
-      this.isDragging = false;
-
-  // Check if an object was intersected and zoom in if it was
-      if (this.intersectedObject) {
-        const position = this.intersectedObject.position.clone();
-        const distance = this.camera.position.distanceTo(position);
-        const duration = Math.min(Math.max(distance / 2000, 1), 2);
-
-        this.camera.position.x = position.x;
-        this.camera.position.y = position.y;
-        this.camera.position.z = position.z;
-
-        this.intersectedObject = null;
-      }
-    },
-
-    render() {
-  // Call render in a loop
-      requestAnimationFrame(this.render);
-
-  // Update the controls and render the scene
-      this.controls.update();
-      this.renderer.render(this.scene, this.camera);
-    },
-  };
+    // Add click event listener to the star mesh
+    starMesh.addEventListener('click', () => {
+      // TODO: Expand into solar system
+    })
+    // Render the scene
+    this.animate = () => {
+      requestAnimationFrame(this.animate)
+      this.renderer.render(this.scene, this.camera)
+    }
+    this.animate()
+  }
 }
 </script>
 
 <style scoped>
   div {
-    max-width: 100%;
-    max-height: 90vh;
+    width:100%;
+    height:90vh;
     overflow:hidden;
   }
 </style>
