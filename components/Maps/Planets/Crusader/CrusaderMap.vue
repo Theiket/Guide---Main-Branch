b1f6c9585ec799531ec2e7c46c428e6367bb597a
Theiket <85735140+Theiket@users.noreply.github.com>
2023-03-08 18:11:11 +1000

    1 file modified

--- components/Maps/Planets/Crusader/CrusaderMap.vue
+++ components/Maps/Planets/Crusader/CrusaderMap.vue
@@ -1,125 +1,258 @@
 <template>
-  <div ref="container">
-    <canvas ref="canvas"></canvas>
+  <div class="container">
+    <div class="systemName">
+      <NuxtLink to="/MapStanton">
+        <h1>CRUSADER</h1>
+      </NuxtLink>
+    </div>
+    <div class="solar-system">
+      <div class="orbit yela-orbit">
+        <div class="planet yela">
+        <br>
+        <p>Yela</p>
+        </div>
+      </div>
+      <div class="orbit daymar-orbit">
+        <div class="planet daymar">
+        <br>
+        <p>Daymar</p>
+        </div>
+      </div>
+      <div class="orbit cellin-orbit">
+        <div class="planet cellin">
+        <br>
+        <p>Cellin</p>
+        </div>
+      </div>
+      <NuxtLink to="/MapStanton">
+        <div class="crusader"></div>
+      </NuxtLink>
+    </div>
   </div>
 </template>
 
+
 <script>
-import * as THREE from 'three';
-
-export default {
-  data() {
-    return {
-      objects: [
-        { name: 'Stanton', planets: ['Hurston', 'Crusader', 'ArcCorp', 'microtech'] },
-        { name: 'Pyro', planets: ['Pyro I', 'Pyro II', 'Pyro III', 'Pyro IV', 'Pyro V', 'Pyro VI'] },
-        { name: 'Nyx', planets: ['Delamar'] },
-      ],
-      camera: null,
-      renderer: null,
-      scene: null,
-      spheres: [],
-      solarSystem: null,
-    }
-  },
-  mounted() {
-    this.init()
-    this.animate()
-  },
-  methods: {
-    init() {
-    // Create the scene, camera, and renderer
-      this.scene = new THREE.Scene()
-      this.scene.background = new THREE.Color(0x24262B)
-      this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-      this.camera.position.z = 50
-      this.camera.position.x = 0
-      this.renderer = new THREE.WebGLRenderer({ canvas: this.$refs.canvas })
-      this.renderer.setSize(window.innerWidth, window.innerHeight)
-      this.renderer.setPixelRatio(window.devicePixelRatio)
-
-    // Add the renderer to the canvas element
-      this.$refs.canvas.appendChild(this.renderer.domElement)
-
-    // Create a container for the solar system
-      this.solarSystem = new THREE.Object3D()
-
-    // Add spheres to the scene based on the objects array
-      this.objects.forEach((object, index) => {
-        const geometry = new THREE.SphereGeometry(5, 32, 32)
-        const material = new THREE.MeshStandardMaterial({ color: 0xffffff })
-        const sphere = new THREE.Mesh(geometry, material)
-        sphere.position.set(-15 + index * 15, 0, 0)
-        sphere.name = object.name
-        sphere.userData.planets = object.planets // Store planets array as user data
-        this.scene.add(sphere)
-        this.spheres.push(sphere)
-      })
-
-    // Add event listener for clicking on spheres
-      this.renderer.domElement.addEventListener('click', this.onClick.bind(this))
-    },
-    animate() {
-      requestAnimationFrame(this.animate.bind(this))
-      this.spheres.forEach((sphere) => {
-        sphere.rotation.y += 0.01
-      })
-      this.renderer.render(this.scene, this.camera)
-    },
-    onClick(event) {
-    // Check if a sphere was clicked
-      const raycaster = new THREE.Raycaster()
-      const mouse = new THREE.Vector2()
-      mouse.x = (event.clientX / window.innerWidth) * 2 - 1
-      mouse.y = -(event.clientY / window.innerHeight) * 2 + 1
-      raycaster.setFromCamera(mouse, this.camera)
-      const intersects = raycaster.intersectObjects(this.spheres)
-
-      if (intersects.length > 0) {
-      // Update the camera position to look at the clicked sphere
-        const clickedSphere = intersects[0].object
-        const distance = clickedSphere.position.distanceTo(this.camera.position)
-        const direction = this.camera.position.clone().sub(clickedSphere.position).normalize()
-        const offset = direction.multiplyScalar(distance * 0.5)
-        this.camera.position.copy(clickedSphere.position.clone().add(offset))
-        this.camera.lookAt(clickedSphere.position)
-
-      // Remove the event listener for clicking on spheres
-        this.renderer.domElement.removeEventListener('click', this.onClick.bind(this))
-
-      // Remove the spheres from the scene
-        this.solarSystem.remove(...this.spheres)
-
-      // Create the solar system for the clicked object
-        const planets = clickedSphere.userData.planets
-        const sphereInstances = []
-        const sphereGeometry = new THREE.SphereGeometry(5, 32, 32)
-        const sphereMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff })
-
-        for (let i = 0; i < planets.length; i++) {
-          const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
-          sphere.position.set(10 * i, 0, 0)
-          sphere.name = planets[i]
-          sphereInstances.push(sphere)
-        }
-
-        const instanceGroup = new THREE.Group()
-        instanceGroup.add(...sphereInstances)
-        this.solarSystem.add(instanceGroup)
-
-      // Add the solar system to the scene and the spheres array
-        this.scene.add(this.solarSystem)
-        this.spheres = sphereInstances
-      }
-    }
-  },
 </script>
 
 <style scoped>
-  div {
-    max-height:90vh;
-    margin:0;
-    margin-inline-start:-1.5%;
-    overflow:hidden;
-  }
+/* General */
+  .container {
+    user-select:none;
+    }
+  .systemName {
+    animation: 1.5s slideappear;
+    }
+
+  .solar-system {
+    width: 250px;
+    height: 250px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    scale:285%;
+    position:absolute;
+    margin-block-start:20%;
+    margin-inline-start:40%;
+    }
+
+/* Orbit */
+  .orbit {
+    position: absolute;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid var(--lightorange);
+    border-radius: 50%;
+    z-index:-1;
+    }  
+  .yela-orbit {
+    width: 230px;
+    height: 230px;
+    rotate:320deg;
+    }
+  .daymar-orbit {
+    width: 180px;
+    height: 180px;
+    rotate:40deg;
+    }
+  .cellin-orbit {
+    width: 150px;
+    height: 150px;
+    rotate:210deg;
+    }
+/* Moons */
+  .planet {
+    position: absolute;
+    top: -5px;
+    width: 10px;
+    height: 10px;
+    border-radius: 50%;
+    background-color: #3ff9dc;
+    z-index:1;
+    }
+  .planet.cellin {
+    background-color:#EEE3F1;
+    rotate:-210deg;
+    z-index:1;
+    }
+  .planet.daymar {
+    background-color:#A2793A;
+    rotate:-40deg;
+    z-index:1;
+    }
+  .planet.yela {
+    background-color:#6B6A6A;
+    rotate:-320deg;
+    }
+  .planet:hover {
+    box-shadow:0px 0px 5px black;
+    top:-6.5px;
+    width:13px;
+    height:13px;
+    cursor:pointer;
+    }
+  .planet p {
+    visibility:hidden;
+    rotate:0deg;
+    margin-block-start:-3px;
+    }
+  .planet:hover p {
+    color:var(--lightgray);
+    visibility:visible;
+    }
+  .planet.cellin p {
+    margin-inline-start:-15px;
+    }
+  .planet.daymar p {
+    margin-inline-start:-20px;
+    margin-block-start:-5px;
+    }
+  .planet.yela p {
+    margin-inline-start:-14px;
+    }
+/* Crusader */
+  .crusader {
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+    background-color: #6C4975;
+    z-index:1;
+    }
+  .crusader:hover {
+    box-shadow:0px 0px 5px black;
+    width:35px;
+    height:35px;
+    cursor:pointer;
+    }
+
+/* Text */
+  .systemName {
+    color: var(--lightorange);
+    letter-spacing:1px;
+    margin-block-start:0px;
+    text-decoration:none;
+    display:inline-block;
+    width:15%;
+    position:absolute;
+    margin-inline-start:20%;
+    margin-block-start:5%;
+    border-bottom-style:solid;
+    border-width:2px;
+    border-color:var(--lightgray);
+    padding-inline-start:10px;
+    z-index:1;
+    }
+  .systemName h1 {
+    font: 35px 'Segoe UI', sans-serif;
+    font-weight:bold;
+    }
+  .systemName a {
+    color:var(--lightorange);
+    }
+  .systemName a:hover {
+    color:var(--orangehover);
+    }
+
+  p {
+    font-weight: 300;
+    font-family: 'Segoe UI', sans-serif;
+    font-size: 5px;
+    font-weight:bold;
+    letter-spacing:0.5px;
+    color: var(--lightgray);
+    padding-block-end: 15px;
+    padding-inline-start:15px;
+    padding-inline-end:15px;
+    }
+
+/* Animations */
+  @keyframes slideappear {
+    0% {
+      opacity: 0;
+      left:-500px;
+      }
+    75% {
+      opacity:0;
+      }
+    100% {
+      opacity: 1;
+      left:25px;
+      }
+    }
+  @keyframes appear1 {
+    0% {
+      opacity: 0;
+      scale:0;
+      }
+    60% {
+      opacity:0;
+      scale:0.6;
+      }
+    }
+  @keyframes appear2 {
+    0% {
+      opacity: 0;
+      scale:0;
+      }
+    60% {
+      opacity:0;
+      scale:0.6;
+      rotate:0deg;
+      }
+    }
+  @keyframes appear3 {
+    0% {
+      opacity: 0;
+      scale:0;
+      }
+    60% {
+      opacity:0;
+      scale:0.6;
+      rotate:0deg;
+      }
+    }
+  @keyframes appear4 {
+    0% {
+      opacity: 0;
+      scale:0;
+      }
+    60% {
+      opacity:0;
+      scale:0.6;
+      rotate:0deg;
+      }
+    }
+  .crusader {
+    animation: 1s appear1;
+    }
+  .cellin-orbit {
+    animation: 2s appear2;
+    }
+  .daymar-orbit {
+    animation: 3s appear3;
+    }
+  .yela-orbit {
+    animation: 4s appear4;
+    }
 </style>
